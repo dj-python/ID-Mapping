5349067e52f007f8bfbb3f63af08cb5e4c2ea4ef
dj-python <pdj911@naver.com>
2025-02-02 22:22:26 +0900

    Update Def test code.py

--- Def test code.py
+++ Def test code.py
@@ -1,15 +1,30 @@
-f = open('info.txt', 'r')
-lines = f.readlines()
-f.close()
-c = lines[4][-3:-1]
-d = lines[5][-3:-1]
-e = lines[6][-5:-1]
-f = lines[7][-5:-1]
+
+with open('Sensor_Info.txt', 'r') as f:
+    lines = f.readlines()
+
+print(lines[4])
+
+if lines[4][-4] == 'x':
+    d = lines[5][-3:-1]
+    print(d)
 
 
 
-print(lines)
-print(c)
-print(d)
-print(e)
-print(f)
+# 0x 이후의 문자를 인덱스 하려면 아래의 방법이 좋다
+
+with open('Sensor_Info.txt', 'r') as f:
+    lines = f.readlines()
+
+print(lines[4])
+
+# '0x' 가 포함된 줄인지 확인
+if '0x' in lines[4]:
+    # '0x' 이후의 두 문자 추출
+    start_index = lines[4].index('0x')
+    d = lines[4][start_index:start_index + 2]
+    print(d)
+else :
+    print('조건이 충족되지 않았습니다')
+
+
+
